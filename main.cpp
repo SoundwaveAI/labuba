ced8563c167e19b2f1a835523baa9b3b288f8612
SoundwaveAI <ghost10.town10000@gmail.com>
2025-10-20 12:57:17 +0300

    dobavili 2 function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 int ** create(size_t rows, size_t cols);
 void remove(int ** m, size_t rows, size_t cols);
-void input(int ** m, size_t rows, size_t cols);
-void output(const int * const * m, size_t rows, size_t cols);
+void input(int ** m, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows &&(std::cin); ++i) {
+    for (size_t j = 0; j < cols && (std::cin), ++j) {
+      std::cin >> m[i][j];
+    }
+  }
+}
+void output(const int * const * m, size_t rows, size_t cols) {
+  std::cout << rows << cols;
+}
 int main() {
   size_t rows = 0, cols = 0;
   std::cin >> rows >> cols;
